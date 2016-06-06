9b20b8fd09a4501de1e0a3891dc9d38b293846f2
Ovidiu Miron <omiron@mirantis.com>
2016-06-06 07:36:29 +0100

    Add om-antfs-cli

--- zsh/custom/om.zsh
+++ zsh/custom/om.zsh
@@ -8,3 +8,9 @@ alias om-scp-resume="rsync --partial --progress --rsh=ssh $1"
 alias om-arch-upgrade="sudo pacman -Syu"
 
 function om-del-line () { sed -i $1d $2}
+
+alias om-antfs-cli="
+until  antfs-cli; do
+    echo '-------------->Antfs-cli returns error. Try again !'
+    sleep 1
+done"
